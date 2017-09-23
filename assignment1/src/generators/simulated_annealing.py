4f42a5087826dbff6793b04cc08725aaa9d98c0a
Hans <kiessler.hans@gmail.com>
2017-09-23 18:36:00 -0400

    Thought I added the previous change to master

--- assignment1/src/generators/simulated_annealing.py
+++ assignment1/src/generators/simulated_annealing.py
@@ -14,19 +14,22 @@ class SimulatedAnnealingGenerator:
 		# p = self.puzzle
 		temp = start_temp
 		for n in range(0, iters):
-			old_value = self.puzzle.value()
 			# make a random change
 			x, y, old = self.puzzle.change_random_entry()
 			new_value = self.puzzle.value()
 			print('new_value= ' + str(new_value) + '; old_value= ' + str(old_value) + '; temp= ' + str(temp))
-			acceptance_prob = math.exp((new_value - old_value)/temp)
 			if temp < 0.00001:
 				break
+			acceptance_prob = math.exp((new_value - old_value)/temp)
+
 			# if new_value > old_value or random.uniform(0, 1) < acceptance_prob:
 			if new_value <= old_value or random.uniform(0, 1) >= acceptance_prob:
 
 				self.puzzle[x,y] = old
 				# probably not this
 				# self.puzzle = p
+			else:
+				old_value = self.puzzle.value()
+
 			temp = temp * decay_rate
 		return (self.puzzle.clone_grid(), self.puzzle.value())
