b0167ca3962ee72402a64ce534e03db74c798a3d
Harry Stern <hcs@meow.sh>
2017-09-26 18:31:50 -0400

    Fix install script to work on ilabs

--- assignment1/install.sh
+++ assignment1/install.sh
@@ -1,5 +1,8 @@
-python3 -m venv .
+python3 -m venv --without-pip .
 source bin/activate
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+rm get-pip.py
 pip install -r requirements.txt
 echo
 echo 'Dependencies installed. Run `source bin/activate` to enable virtual environment.'
