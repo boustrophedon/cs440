ea3ca14c41887ad501a284d3b43cdca45ea62dad
Harry Stern <hcs@meow.sh>
2017-11-18 02:10:49 -0500

    Add test for htt terrain and remove extra print statements

--- assignment3/src/test.py
+++ assignment3/src/test.py
@@ -41,6 +41,7 @@ def test_search_neighbors():
             print("Test passed.")
         else:
             print("Test failed. Path:", ' '.join(map(str, result)))
+            print("Expected:", ' '.join(map(str, expected)))
     else:
         print("Test failed, no path found.")
 
@@ -62,6 +63,7 @@ def test_search_obstacle():
             print("Test passed.")
         else:
             print("Test failed. Path:", ' '.join(map(str, result)))
+            print("Expected:", ' '.join(map(str, expected)))
     else:
         print("Test failed, no path found.")
 
@@ -79,8 +81,34 @@ def test_search_one_away():
             print("Test passed.")
         else:
             print("Test failed. Path:", ' '.join(map(str, result)))
+            print("Expected:", ' '.join(map(str, expected)))
     else:
         print("Test failed, no path found.")
+
+def test_search_htt():
+    print("Test HTT terrain")
+    grid = NavigationGrid(10, 2)
+    grid.start = (0,0)
+    grid.goal = (9,0)
+    for i in range(1, 9):
+        grid[i,0] = HTT
+
+
+    search = GraphSearch(grid)
+    result = search.search()
+    expected = [grid.goal,]
+    for i in range(8, 0, -1):
+        expected.append((i, 1))
+    expected.append(grid.start)
+    if result is not None:
+        if result == expected:
+            print("Test passed.")
+        else:
+            print("Test failed. Path:", ' '.join(map(str, result)))
+            print("Expected:", ' '.join(map(str, expected)))
+    else:
+        print("Test failed, no path found.")
+
 
 if __name__ == '__main__':
     test_search_nopath()
@@ -90,4 +118,6 @@ if __name__ == '__main__':
     test_search_obstacle()
     print()
     test_search_one_away()
+    print()
+    test_search_htt()
     print()
