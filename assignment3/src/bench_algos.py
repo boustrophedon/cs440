9c24fdc4beddb0a89f0aa6ada591d1ce05e04a24
Harry Stern <hcs@meow.sh>
2017-11-27 01:03:19 -0500

    Move maps data, add bench script, add collected bench data

---
+++ assignment3/src/bench_algos.py
@@ -0,0 +1,66 @@
+import os
+import time
+
+import csv
+
+from navigation_grid import *
+from graph_search import *
+from sequential_search import *
+
+fields = ['map', 'algo', 'path_cost', 'runtime', 'heuristic', 'weight']
+heuristics = {empty_heuristic:"ucs", euclidean_admissible:"euclid_adm", \
+        manhattan:"manhattan", chebyshev:"chebyshev", euclidean:"euclidean", \
+        dist_squared:"dist_squared"}
+
+def run_bench(m, grid):
+    output = list()
+
+    weights = [1.0, 1.5, 2.0]
+
+    for heuristic,hname in heuristics.items():
+        for weight in weights:
+            data = dict()
+            if heuristic == empty_heuristic and weight != 1.0:
+                continue
+
+            start = time.time()
+
+            searcher = GraphSearch(grid, heuristic=heuristic, weight=weight)
+            path_cost = grid.path_cost(searcher.search()) 
+
+            end = time.time()
+            
+
+            runtime = end-start
+
+            data['map'] = m
+            if heuristic == empty_heuristic:
+                data['algo'] = 'ucs'
+            elif weight == 1.0:
+                data['algo'] = 'astar'
+            else:
+                data['algo'] = 'weighted_astar'
+            data['path_cost'] = path_cost
+            data['runtime'] = runtime
+            data['heuristic'] = hname
+            data['weight'] = weight
+
+            output.append(data)
+
+    return output
+
+def main():
+    maps = os.listdir("maps")
+
+    data = list()
+    for m in maps:
+        grid = NavigationGrid.from_file("maps/"+m)
+        data += run_bench(m, grid)
+
+    with open('data/bench_data.csv', 'w', newline='') as output:
+        bench_out = csv.DictWriter(output, fieldnames=fields)
+        for d in data:
+            bench_out.writerow(d)
+
+if __name__ == '__main__':
+    main()
